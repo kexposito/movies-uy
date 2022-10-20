7a9a4bb4a8dc647bebddad7e0dcf23c862b11210
Kevin Exposito <kevin@mimiquate.com>
2022-10-20 13:17:47 -0300

    Add ExTwitter and Finch

--- mix.exs
+++ mix.exs
@@ -48,7 +48,10 @@ defmodule MoviesUy.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:finch, "~> 0.13"},
+      {:oauther, "~> 1.1"},
+      {:extwitter, "~> 0.12"}
     ]
   end
 
